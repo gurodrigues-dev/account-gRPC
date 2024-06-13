4b8f0da906d8ac18b5c8427ccd30c2b3c173b9e6
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-12 22:52:33 -0300

    feat: config

---
+++ config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Name     string `yaml:"name"`
+	Database Database
+	Server   Server
+}
+
+type Server struct {
+	Host   string `yaml:"host"`
+	Port   int    `yaml:"port"`
+	Secret string `yaml:"string"`
+}
+
+type Database struct {
+	User     string `yaml:"dbuser"`
+	Port     string `yaml:"dbport"`
+	Host     string `yaml:"dbhost"`
+	Password string `yaml:"dbpassword"`
+	Name     string `yaml:"dbname"`
+	Schema   string `yaml:"schema"`
+}
+
+var config *Config
+
+func Load(filename string) (*Config, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var conf Config
+	if err := yaml.Unmarshal(data, &conf); err != nil {
+		return nil, err
+	}
+
+	config = &conf
+	return config, nil
+}
+
+func Get() *Config {
+	return config
+}
